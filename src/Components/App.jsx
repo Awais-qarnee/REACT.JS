5757dc4118dbe3ad3f6027a0813381b52b5c98d8
Awais-qarnee <awaisimran31dec@gmail.com>
2023-11-28 12:28:10 +0500

    add files

---
+++ src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Props from './Components/Nav/Props'
+
+const App = () => {
+    return (
+        <>
+            <Props/>
+        </>
+    )
+}
+
+export default App
---
+++ src/Components/Nav/Props.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Props.css'
+
+const Props = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Props
